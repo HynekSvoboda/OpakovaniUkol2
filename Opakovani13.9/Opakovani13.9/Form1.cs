819943e93b290662af2b1e1bed1fa534e61eba38
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-09-13 11:24:46 +0200

    Add files via upload

---
+++ Opakovani13.9/Opakovani13.9/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opakovani13._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text1 = textBox1.Text;
+            string text2 = textBox2.Text;
+            DateTime datum = dateTimePicker1.Value;
+
+            Zamestnanec zam = new Zamestnanec(text2, text1, 0, datum);
+            MessageBox.Show(zam.ToString());
+        }
+    }
+}
---
+++ Opakovani13.9/Opakovani13.9/Zamestnanec.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opakovani13._9
+{
+    class Zamestnanec
+    {
+        string titul;
+        string jmeno;
+        int plat;
+        DateTime datum;
+        int osobnihodnoceni;
+        bool zamestnanec;
+        int odprachodiny;
+
+        string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            set
+            {
+                string[] pole = value.Split(' ');
+                pole[0] = pole[0].ToLower();
+                pole[0] = pole[0].First().ToString().ToUpper()+pole[0].Substring(1);
+                pole[1]= pole[1].ToUpper();
+                jmeno = pole[0]+" "+pole[1];
+            }
+        }
+
+        string Titul
+        {
+            get
+            {
+                return titul;
+            }
+            set
+            {
+                titul = value[0].ToString().First().ToString().ToUpper() + value.Substring(1)+".";
+            }
+        }
+
+        public Zamestnanec(string titul,string jmeno,int plat, DateTime datum)
+        {
+            Titul = titul;
+            Jmeno = jmeno;
+            this.plat = plat;
+            this.datum = datum;
+            osobnihodnoceni = 0;
+            odprachodiny = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Jméno: " +Jmeno.ToString() +" Titul: "+ Titul.ToString();
+        }
+    }
+}
